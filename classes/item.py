1023f53585b48fdfd19608882d831307e7c90a55
Kevin Irias <kevin.irias@inaxir.com>
2016-08-29 13:51:24 -0600

    Se mejoró la estructura de la clase Item.

--- classes/item.py
+++ classes/item.py
@@ -1,6 +1,10 @@
-class Item:
+class Item(object):
     def __init__(self, cantidad, objeto_producto):
         self._cantidad = cantidad
 	self._producto = objeto_producto
     def calcularValorTotalItem(self):
 	return (self._cantidad * self._producto._precioUnitario)
+    def __str__(self):
+	return (str(self._producto) + 
+		'Cantidad solicitada: ' + str(self._cantidad) + '\n' + 
+		'Valor total del item: ' + str(self.calcularValorTotalItem()) +'\n')
