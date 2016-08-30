1684d486f7ea0c2f31dfedb2b4aacf0935d76a9b
Kevin Irias <kevin.irias@inaxir.com>
2016-08-29 22:33:49 -0600

    Limpieza de código y mejoras en la implementación.

--- tarea_uso_de_clases.py
+++ tarea_uso_de_clases.py
@@ -2,54 +2,64 @@ from classes.producto import Producto
 from classes.cliente import Cliente
 from classes.item import Item
 from classes.carritocompra import CarritoCompra
-"""Instanciacion de 4 productos nuevos"""
-print 'CREACION DE PRODUCTOS'
+from classes.empleado import Empleado
+
+"""Objeto instancia de la clase  Cliente"""
+cliente = Cliente('Maria Alejandra Cuadra Hernandez', 20, 'Mujer', 'Casada', 'CL02', True)
+
+"""Objeto instancia de la clase Empleado"""
+empleado = Empleado('Ernesto Enrique Morazan Delgado', 35, 'Hombre', 'Viudo', 'EM01','Gerente general', 7800.00)
+
+print 'Comparacion entre representacion de cadena de un objeto Cliente y un objeto Empleado.'
+print '***Objeto Cliente***'
+print str(cliente)
+cliente.presentarse()
+print ''
+print '***Objeto empleado***'
+print str(empleado)
+empleado.presentarse()
+print ''
+
+"""Instanciacion de 4 productos nuevos"""  
 p1 = Producto('P01', 'Producto de prueba 1', 200)
 p2 = Producto('P02', 'Producto de prueba 2', 300)
 p3 = Producto('P03', 'Producto de prueba 3', 400)
 p4 = Producto('P04', 'Producto de prueba 4', 500)
+
+"""Prueba de acceso a las propiedades de un Producto"""
+print 'Prueba de acceso directo a las propiedades de una instancia de Producto'
+print 'Codigo del producto: ' + p1._codigoProducto
+print 'Descripcion: ' + p1._descripcion
+print 'Precio Unitario: ' + str(p1._precioUnitario) + '\n'
+
 """Se reduce el precio del producto 1 en 50"""
 p1.reducirPrecio(50)
 
-print "" 
-"""Prueba de acceso a las propiedades de un Producto"""
-print 'Accessando a las propiedades del primer objeto de tipo Producto instanciado'
-print 'Codigo del producto: ' + p1._codigoProducto
-print 'Descripcion: ' + p1._descripcion
-print 'Precio Unitario: ' + str(p1._precioUnitario)
+print 'Representacion de cadena de 4 instancias de la clase Producto'
+print 'Producto #1' + '\n' + str(p1)
+print 'Producto #2' + '\n' + str(p2)
+print 'Producto #3' + '\n' + str(p3)
+print 'Producto #4' + '\n' + str(p4)
 
-print""
 """Creo e instancio un nuevo cliente"""
 cl = Cliente('Kevin Alexander Irias Hernandez', 19, 'Hombre', 'Soltero', 'CL01', True )
-cl.presentarse()
-"""Datos del nuevo cliente creado"""
-print 'Datos del objeto cliente instanciado'
-print cl
 
 """Instanciacion del objeto Carrito de Compras"""
 carrito = CarritoCompra(cl)
-"""Se agregan items al carrito"""
+"""Se agregan 4 productos al carrito"""
 carrito.agregarItem(3, p1)
 carrito.agregarItem(4, p2)
 carrito.agregarItem(5, p3)
 carrito.agregarItem(6, p4)
 
-print ""
-print 'Detalle del carrito antes de eliminar items: '
-print carrito.obtenerDetalleCarrito()
-print 'Resumen de montos del carrito antes de eliminar items:'
-print carrito.obtenerResumenMontos()
+print 'Informacion del carrito de compras antes de eliminar items: '
+print carrito
 print carrito.notificarDescuento()
-print ""
+print ''
+
 """Se elimina el producto en la segunda posicion dentro del carrito de compras"""
 carrito.eliminarItem(1)
 
-print 'Informacion del cliente asociado al carrito de compras'
-print cl
-print ""
-print 'Detalle del carrito luego que se quito un item del carrito'
-print carrito.obtenerDetalleCarrito()
-print ""
-print 'Resumen de montos del carrito despues de eliminar un item'
-print carrito.obtenerResumenMontos()
+print 'Informacion del carrito de compras luego que se eliminara un item:'
+print carrito
 print carrito.notificarDescuento()
